6f7248536063c6b494a9a04eb910ba4e08a1d79b
EsotericZ <cjsand03@gmail.com>
2021-10-31 18:18:34 -0600

    created checkboxes in html, created let variables for them in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 // Assignment Code
+
+let length = document.getElementById("length").value
+let lower = document.getElementById("lowercase").checked
+let upper = document.getElementById("uppercase").checked
+let numeric = document.getElementById("numeric").checked
+let special = document.getElementById("special").checked
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
